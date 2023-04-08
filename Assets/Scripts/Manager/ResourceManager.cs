c700e2f56176f84d20885ade5a503f358e6386bb
jmJang00 <50986275+jmJang00@users.noreply.github.com>
2023-04-09 00:54:24 +0900

    리소스 매니저 추가

---
+++ Assets/Scripts/Manager/ResourceManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    static ResourceManager s_instance;
+    static ResourceManager Instance { get { init(); return s_instance; } }
+    int time = 1;
+    GameObject test;
+
+    void Start()
+    {
+        init();
+    }
+
+    void Update()
+    {
+        if (time == 1)
+        {
+            test = Instantiate("Cube");
+            
+        }
+        else if (time == 100)
+        {
+            Destroy(test);
+        }
+        ++time;
+    }
+
+    static void init()
+    {
+        if (s_instance == null)
+        {
+            GameObject go = GameObject.Find("ResourceManager");
+            if (go == null)
+            {
+                go = new GameObject { name = "ResourceManager" };
+                go.AddComponent<ResourceManager>();
+            }
+
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<ResourceManager>();
+        }
+    }
+
+    public T Load<T>(string path) where T : Object
+    {
+        return Resources.Load<T>(path);
+    }
+
+    public GameObject Instantiate(string path, Transform parent = null)
+    {
+        GameObject prefab = Load<GameObject>($"Prefabs/{path}");
+        if (prefab == null)
+        {
+            Debug.Log($"Failed to load prefab : {path}");
+            return null;
+        }
+        return Object.Instantiate(prefab, parent);
+    }
+
+    public void Destroy(GameObject go)
+    {
+        if (go == null)
+            return;
+        Object.Destroy(go);
+    }
+}

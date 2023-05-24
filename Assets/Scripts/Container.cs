3c35cb191d1e8923127e2a320cd3c09b6ee8ce29
longZam <longZam305@gmail.com>
2023-05-24 21:20:32 +0900

    voxel system과 crane controller 연동

--- Assets/Scripts/Container.cs
+++ Assets/Scripts/Container.cs
@@ -1,7 +1,22 @@
 using UnityEngine;
+using System;
 
 
-public class Container : PlaceableObject
+public class Container : PlaceableObject, IContainerInfo
 {
+    public string Code
+    {
+        get => code;
+        set => code = value;
+    }
 
+    // todo: 컨테이너 사이즈는 Vector3Int임
+    Vector2 IContainerInfo.Size
+    {
+        get => throw new NotImplementedException();
+        set => throw new NotImplementedException();
+    }
+
+
+    private string code;
 }
